3469268c4a8db9ee3f261f1c2ac00cb2633825f3
Liu ChuanFu(集团商业部) <chuanfuliu217412@sohu-inc.com>
2019-09-04 23:17:57 +0800

    修改为less

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '@/reducers';
-import './index.css';
+import './index.less';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
